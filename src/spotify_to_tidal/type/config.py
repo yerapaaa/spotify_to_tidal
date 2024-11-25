257809266d168801230edcac7f789de7f8a3f64a
yerapaaa <sergio_2389@hotmail.com>
2024-11-25 19:07:29 +0100

    Update config.py

--- src/spotify_to_tidal/type/config.py
+++ src/spotify_to_tidal/type/config.py
@@ -2,10 +2,10 @@ from typing import TypedDict, Literal, List, Optional
 
 
 class SpotifyConfig(TypedDict):
-    client_id: str
-    client_secret: str
-    username: str
-    redirect_url: str
+    client_id: 02f543ba2d854feebfb3503e3e1f092f
+    client_secret: f385474c380b4e078d4bb94a09623e7c
+    username: Yerapaaa
+    redirect_url: developer.spotify.com
 
 
 class TidalConfig(TypedDict):
